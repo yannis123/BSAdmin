015b109b3d6a9091c6dd6b9b6dca11b3131884ee
yanghaitao <Haitao.Yang@partner@bmw.com>
2017-10-18 15:10:28 +0800

    modify

--- src/client/web/Installers/Installers/ServiceInstaller.cs
+++ src/client/web/Installers/Installers/ServiceInstaller.cs
@@ -14,13 +14,21 @@ namespace web.Ioc.Installers
     {
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
+            //Castle容器的组件生存周期，主要有如下几种
+            //1.Singleton: 容器中只有一个实例将被创建
+            //2.Transient : 每次请求创建一个新实例
+            //3.PerThread: 每线程中只存在一个实例
+            //4.PerWebRequest : 每次web请求创建一个新实例
+            //5.Pooled ：使用"池化"方式管理组件，可使用PooledWithSize方法设置池的相关属性
+
             container.Register(Component.For<IServiceconfiguration>() //接口  
-                   .ImplementedBy<Serviceconfiguration>() //实现类 
+                   .ImplementedBy<Serviceconfiguration>()
+                   .LifestyleSingleton() //实现类 
              );
 
             container.Register(Component.For<IPersonService>() //接口  
                   .ImplementedBy<PersonService>() //实现类 
-                  .DependsOn(Dependency.OnValue("connectionString", "DefaultConnection"))//构造函数参数
+                  .LifestyleSingleton()
             );
            
         }
