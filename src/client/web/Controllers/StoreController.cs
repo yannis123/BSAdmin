3238154d4310ee519d0997aac5b7dfa063c7bc89
ysnnis <yannis_123@live.com>
2018-01-09 22:40:00 +0800

    添加店铺列表

--- src/client/web/Controllers/StoreController.cs
+++ src/client/web/Controllers/StoreController.cs
@@ -1,17 +1,26 @@
-﻿using System;
+﻿using Domain.IService;
+using Domain.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using web.Models.RequestModel;
 
 namespace web.Controllers
 {
     public class StoreController : BaseController
     {
+        private IMRKeHuService _kehuService = null;
+        public StoreController(IMRKeHuService kehuService)
+        {
+            _kehuService = kehuService;
+        }
         // GET: Store
         public ActionResult Index()
         {
-            return View();
+            List<MRKeHu> list = _kehuService.GetKeHuList();
+            return View(list);
         }
     }
 }
---
+++ src/core/Domain/Service/MRKeHuService.cs
@@ -0,0 +1,40 @@
+﻿using Domain.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DapperExtensions;
+using Dapper;
+using System.Data.SqlClient;
+using System.Data;
+using Domain.Model;
+
+namespace Domain.Service
+{
+    public class MRKeHuService : IMRKeHuService
+    {
+        private IDbConnection connection;
+        public MRKeHuService(IDBConnectionManager connManager)
+        {
+            connection = connManager.GetDefaultConn();
+        }
+        public List<Model.MRKeHu> GetKeHuList()
+        {
+            string sql =@"select 
+                    [MR_KEHU].khdm,
+                    [MR_KEHU].khmc,
+                    [MR_KEHU].qydm,
+                    [MR_KEHU].qddm,
+                    [MR_QUDAO].qdmc,
+                    [MR_QUYU].qymc 
+                    from [dbo].[MR_KEHU] 
+                    left join [dbo].[MR_QUDAO]
+                    on [MR_KEHU].qddm=[MR_QUDAO].qddm
+                    left join [dbo].[MR_QUYU]
+                    on [MR_KEHU].qydm=[MR_QUYU].qydm
+                    where khdm='I57133'";
+            return connection.Query<MRKeHu>(sql).ToList<MRKeHu>();
+        }
+    }
+}
