3613d1fd4c0d5292bc7c44028e83b4babf5b8a58
yanghaitao <Haitao.Yang@partner@bmw.com>
2018-03-06 17:14:19 +0800

    add jobmanager

---
+++ src/client/QuartzJobManager/TestJob.cs
@@ -0,0 +1,19 @@
+﻿using Common.Logging;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartzJobManager
+{
+    public class TestJob : IJob
+    {
+        private readonly ILog _logger = LogManager.GetLogger(typeof(QuartzJobManager.TestJob));
+        void IJob.Execute(IJobExecutionContext context)
+        {
+            _logger.InfoFormat("TestJob测试");
+        }
+    }
+}
